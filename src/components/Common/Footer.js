d6c7d72a377f529357d2e2b9c9969d12a651ef33
Yen Kuo <u7075575@anu.edu.au>
2021-06-14 17:50:33 +1000

    add footer

--- src/components/Common/Footer.js
+++ src/components/Common/Footer.js
@@ -3,11 +3,35 @@ import "./Footer.css";
 
 function footer() {
   return (
-    <div>
+    <div className="footer">
+      <div className="icon">
+        <a href="https://www.facebook.com/profile.php?id=100001234828807">
+          <i className="fab fa-facebook-square iconitem"></i>
+        </a>
+        <a href="https://www.instagram.com/kuoyen311/">
+          <i className="fab fa-instagram iconitem"></i>
+        </a>
+        <a href="https://github.com/yen311">
+          <i className="fab fa-github-square iconitem"></i>
+        </a>
+        <a href="https://www.linkedin.com/in/yen-kuo/">
+          <i className="fab fa-linkedin iconitem"></i>
+        </a>
+        <a href="mailto:kuoyen5@gmail.com">
+          <i className="fas fa-envelope-square iconitem"></i>
+        </a>
+      </div>
       <div>
-        {/* <a>
-          <i class="fab fa-facebook-square">i</i>
-        </a> */}
+        <p className="copyright">
+          <i class="far fa-copyright"></i>
+          Yen_Kuo
+          <a
+            className="copyright-email"
+            href="yen311.github.io/personal-website/"
+          >
+            yen311.github.io/personal-website/
+          </a>
+        </p>
       </div>
     </div>
   );
