522be6b766fbef2261375468f2562f7754057dd7
Yen Kuo <u7075575@anu.edu.au>
2021-08-04 02:43:25 +1000

    dynamic email added

--- src/components/Contact/ContactContent.js
+++ src/components/Contact/ContactContent.js
@@ -1,9 +1,48 @@
-import React, { useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import Card from "../UI/Card";
 import FooterIcon from "../Common/FooterIcon";
 
 function ContactContent() {
-  const email = "kuoyen5@gmail.com";
+  const emailArr = [
+    "K",
+    "u",
+    "o",
+    "y",
+    "e",
+    "n",
+    "5",
+    "@",
+    "g",
+    "m",
+    "a",
+    "i",
+    "l",
+    ".",
+    "c",
+    "o",
+    "m",
+  ];
+  const [reverse, setReverse] = useState(false);
+  const [count, setCount] = useState(0);
+  const [email, setEmail] = useState("");
+  useEffect(() => {
+    const interval = setInterval(() => {
+      if (!reverse) {
+        setEmail(email + emailArr[count]);
+        setCount(count + 1);
+        if (count >= 16) {
+          setReverse(true);
+        }
+      } else {
+        setEmail(email.slice(0, -1));
+        setCount(count - 1);
+        if (count <= 1) {
+          setReverse(false);
+        }
+      }
+    }, 200);
+    return () => clearInterval(interval);
+  }, [email, count, reverse]);
 
   return (
     <Card className="card-center">
@@ -12,6 +51,7 @@ function ContactContent() {
         <p className="card-top-p">
           You can reach me out from any media list below or email:)
         </p>
+        <p>{email}</p>
       </div>
       <div className="card-bottom">
         <div className="card-botttom-p">
